d6604170229e11c92db21d38788899c47ae22b65
Will Olson <will.olson11@gmail.com>
2021-07-10 15:19:39 -0700

    Hook up the cancel button on the add location screen

---
+++ On the Map/Controllers/AddLocationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddLocationViewController.swift
+//  On the Map
+//
+//  Created by Will Olson on 7/10/21.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+}

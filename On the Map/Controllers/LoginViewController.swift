d4545b734633e6350594a26ad16a060bbac012cd
Will Olson <will.olson11@gmail.com>
2021-07-10 08:49:40 -0700

    Connected login screen outlets and actions to controller

--- On the Map/Controllers/LoginViewController.swift
+++ On the Map/Controllers/LoginViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    // MARK: Lifecycle overrides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
 
-
+    // MARK: Actions
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
 }
